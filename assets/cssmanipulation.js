1ba7bc0014fcfb0b3d7b13a9a5573b4c2b96e811
Jon Volden <volden.jon@gmail.com>
2019-02-26 16:35:45 -0600

    Added the width box. oops

--- assets/cssmanipulation.js
+++ assets/cssmanipulation.js
@@ -5,6 +5,7 @@ function setCSS()
     let borderRed = document.getElementById("borderRed").value
     let borderGreen = document.getElementById("borderGreen").value
     let borderBlue = document.getElementById("borderBlue").value
+    let borderWidth = document.getElementById("borderWidth").value
     let backgroundRed = document.getElementById("backgroundRed").value
     let backgroundGreen = document.getElementById("backgroundGreen").value
     let backgroundBlue = document.getElementById("backgroundBlue").value
@@ -19,12 +20,15 @@ function setCSS()
                            "," + backgroundGreen + 
                            "," + backgroundBlue + ")"
 
-    console.log(borderColor, backgroundColor)
+    if (borderWidth > 20) {
+      document.getElementById("borderWidth").value = 20;
+      borderWidth = 20;
+    }
 
     let elements = document.getElementsByClassName("lorem")
     console.log(elements)
     for (let i = 0; i < elements.length; i++) {
-        elements[i].style.border = "2px solid " + borderColor
+        elements[i].style.border = borderWidth + "px solid " + borderColor
         elements[i].style.backgroundColor = backgroundColor
     }
 
