176ef8d72238d0d785e06c011b92ea342c5496a8
Jon Volden <volden.jon@gmail.com>
2019-02-27 13:10:24 -0600

    Refactored javascript code to handle previous/next functionality

--- assets/pictures.js
+++ assets/pictures.js
@@ -1,26 +1,12 @@
-
 // Rotate pictures
 
-function nextPicture() {
+function setPicture(n) {
   let pictureNumber = document.getElementById("picture")
   let pictureText = document.getElementById("pictureText")
-  let nextFile = parseInt(pictureNumber.src.charAt(pictureNumber.src.length - 5)) + 1
-  if (nextFile == 6) {
-    nextFile = 1
-  }
+  let nextFile = parseInt(pictureNumber.src.charAt(pictureNumber.src.length - 5)) + n
 
-  pictureNumber.src = "images/picture" + nextFile + ".jpg"
-  pictureText.innerHTML = "images/picture" + nextFile + ".jpg"
-}
-
-
-function previousPicture() {
-  let pictureNumber = document.getElementById("picture")
-  let pictureText = document.getElementById("pictureText")
-  let nextFile = parseInt(pictureNumber.src.charAt(pictureNumber.src.length - 5)) - 1
-  if (nextFile == 0) {
-    nextFile = 5
-  }
+  if (nextFile == 0) { nextFile = 5 }
+  if (nextFile == 6) { nextFile = 1 }
 
   pictureNumber.src = "images/picture" + nextFile + ".jpg"
   pictureText.innerHTML = "images/picture" + nextFile + ".jpg"
